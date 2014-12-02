70fca24c7d7d87e3a312c4fc4c22b4c38e8ffe84
zcabjro <Jack@Jacks-MacBook.local>
2014-12-02 17:20:53 +0000

    A couple fixes and interface page
    -Removed a bit of css that was affecting title position
    -Edited a few colours/fonts to make Shabs happy ;)
    -New interface page, accessible through menu with some cheap and
    cheerful text tabs to cycle through designs.

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,7 +1,45 @@
 $(document).ready(function()    {
     $("#my-menu").hide();
+    $("#design2").hide();
+    $("#design3").hide();
     $("#menu-icon").click(function()  {
         $("#my-menu").fadeToggle();
-        console.log("click");
+    });
+    
+    $("#design1Tab").click(function()  {
+        if($("#design2").is(":visible")) {
+            $("#design2").hide();
+        }
+        if($("#design3").is(":visible")) {
+            $("#design3").hide();
+        }
+        $("#design1").show();
+        $("#design1Tab").addClass("my-active");
+        $("#design2Tab").removeClass("my-active");
+        $("#design3Tab").removeClass("my-active");
+    });
+    $("#design2Tab").click(function()  {
+        if($("#design1").is(":visible")) {
+            $("#design1").hide();
+        }
+        if($("#design3").is(":visible")) {
+            $("#design3").hide();
+        }
+        $("#design2").show();
+        $("#design2Tab").addClass("my-active");
+        $("#design1Tab").removeClass("my-active");
+        $("#design3Tab").removeClass("my-active");
+    });
+    $("#design3Tab").click(function()  {
+        if($("#design1").is(":visible")) {
+            $("#design1").hide();
+        }
+        if($("#design2").is(":visible")) {
+            $("#design2").hide();
+        }
+        $("#design3").show();
+        $("#design3Tab").addClass("my-active");
+        $("#design1Tab").removeClass("my-active");
+        $("#design2Tab").removeClass("my-active");
     });
 });
