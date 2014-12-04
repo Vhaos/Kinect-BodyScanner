d8c31b2feceadc9bfd6c208bf50935f2aa9425d0
zcabjro <Jack@Jacks-MacBook.local>
2014-12-04 21:24:01 +0000

    Stubs for pages yet to be done and interface done
    - Created navbars and links for pages yet to be done e.g. Testing
    - Finished editing the interface page and wrote some new content
    bullets for the 3rd design

--- Website/js/main.js
+++ Website/js/main.js
@@ -7,37 +7,37 @@ $(document).ready(function()    {
     });
     
     $("#design1Tab").click(function()  {
+        $("#design1").show();
         if($("#design2").is(":visible")) {
             $("#design2").hide();
         }
         if($("#design3").is(":visible")) {
             $("#design3").hide();
         }
-        $("#design1").show();
         $("#design1Tab").addClass("my-active");
         $("#design2Tab").removeClass("my-active");
         $("#design3Tab").removeClass("my-active");
     });
     $("#design2Tab").click(function()  {
+        $("#design2").show();
         if($("#design1").is(":visible")) {
             $("#design1").hide();
         }
         if($("#design3").is(":visible")) {
             $("#design3").hide();
         }
-        $("#design2").show();
         $("#design2Tab").addClass("my-active");
         $("#design1Tab").removeClass("my-active");
         $("#design3Tab").removeClass("my-active");
     });
     $("#design3Tab").click(function()  {
+        $("#design3").show();
         if($("#design1").is(":visible")) {
             $("#design1").hide();
         }
         if($("#design2").is(":visible")) {
             $("#design2").hide();
         }
-        $("#design3").show();
         $("#design3Tab").addClass("my-active");
         $("#design1Tab").removeClass("my-active");
         $("#design2Tab").removeClass("my-active");
