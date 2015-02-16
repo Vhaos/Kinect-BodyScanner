c07687fcc25150a79209e11bce734b38a57814d4
JackHR <roper.jack.h@gmail.com>
2015-02-16 21:50:16 +0000

    Added PointCloud class and amended Program.cs accordingly
    Small edits to Program.cs
    
    New PointCloud class and methods for buidling, getting centroid,
    translating, rotating and printing out PointCloud

---
+++ BodyScanner/ConsoleKinect/ConsoleKinect/PointCloud.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKinect
+{
+    using System.Windows.Media.Media3D;
+    using System.Globalization;
+
+    class PointCloud
+    {
+        public enum Axis { X, Y, Z };
+        private List<Point3D> points = null;
+        
+        // constructor without pre-built point list
+        public PointCloud(): this(new List<Point3D>()) {}
+        // constructor with pre-built point list
+        public PointCloud(List<Point3D> points)
+        {
+            this.points = points;
+        }
+
+        public List<Point3D> getPoints()
+        {
+            return points;
+        }
+        public Point3D getPoint(int index)
+        {
+            return points[index];
+        }
+        public int getSize()
+        {
+            return points.Count;
+        }
+        // override ToString() method, outputting header followed by each point on a new line
+        public override String ToString()
+        {
+            String header = "ply \n" +
+                                  "format ascii 1.0 \n" +
+                                  "element vertex " + points.Count + "\n" +
+                                  "property float x \n" +
+                                  "property float y \n" +
+                                  "property float z \n" +
+                                  "end_header \n";
+            StringBuilder output = new StringBuilder(header);
+            foreach (Point3D point in points)
+            {
+                output.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2}\n", point.X, point.Y, point.Z));
+            }
+            return output.ToString();
+        }
+        // ToString() using supplied string header
+        public String ToString(String header)
+        {                
+            StringBuilder output = new StringBuilder(header);
+            if (output[output.Length - 1] != '\n')
+            {
+                output.Append('\n');
+            }
+            foreach (Point3D point in points)
+            {
+                output.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2}\n", point.X, point.Y, point.Z));
+            }
+            return output.ToString();
+        }
+
+        public void addPoint(Point3D point)
+        {
+            points.Add(point);
+        }
+
+        // method to calculate and return centroid. We calculate this alongside the point cloud build process to reduce redundancy
+        public Point3D getCentroid()
+        {
+            double xAccumulator = 0.0, yAccumulator = 0.0, zAccumulator = 0.0, xMean = 0.0, yMean = 0.0, zMean = 0.0;
+            float numberOfPoints = (float) points.Count;
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                xAccumulator += points[i].X;
+                yAccumulator += points[i].Y;
+                zAccumulator += points[i].Z;
+            }
+            xMean = xAccumulator / numberOfPoints;
+            yMean = yAccumulator / numberOfPoints;
+            zMean = zAccumulator / numberOfPoints;
+            return (new Point3D(xMean, yMean, zMean));
+        }
+
+        // method that subtracts centroid from point cloud, aligning its center with the origin
+        // NOTE: for repeated operations involving centroid, you should get and store the centroid and supply it to this method as an argument
+        public void subtractCentroid()
+        {
+            int numberOfPoints = points.Count;
+            Point3D centroid = this.getCentroid();
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i] = (Point3D)Point3D.Subtract(points[i], centroid);
+            }
+        }
+        public void subtractCentroid(Point3D centroid)
+        {
+            int numberOfPoints = points.Count;
+            for(int i = 0; i < numberOfPoints; i++)
+            {
+                points[i] = (Point3D)Point3D.Subtract(points[i], centroid);
+            }
+        }
+        public void addCentroid()
+        {
+            Point3D centroid = this.getCentroid();
+            int numberOfPoints = points.Count;
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i] = Point3D.Add(points[i], (Vector3D)centroid);
+            }
+        }
+        public void addCentroid(Point3D centroid)
+        {
+            int numberOfPoints = points.Count;
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                points[i] = Point3D.Add(points[i], (Vector3D)centroid);
+            }
+        }
+        private void applyRotation(Matrix3D rotationMatrix)
+        {
+            int numberOfPoints = points.Count;
+            for(int i = 0; i < numberOfPoints; i++)
+            {
+                points[i] = rotationMatrix.Transform(points[i]); 
+            }
+        }
+        public void rotatePointCloud(double degrees, Axis axis)
+        {
+            double theta = (Math.PI / 180) * degrees;
+            Matrix3D rotationMatrix;
+            switch (axis)
+            {
+                // X-Axis
+                case (Axis.X):
+                    rotationMatrix = new Matrix3D(1, 0, 0, 0,
+                                                     0, Math.Cos(theta), -(Math.Sin(theta)), 0,
+                                                     0, Math.Sin(theta), Math.Cos(theta), 0,
+                                                     0, 0, 0, 1);
+                    break;
+                // Y-Axis
+                case (Axis.Y):
+                    rotationMatrix = new Matrix3D(Math.Cos(theta), 0, -(Math.Sin(theta)), 0,
+                                                     0, 1, 0, 0,
+                                                     Math.Sin(theta), 0, Math.Cos(theta), 0,
+                                                     0, 0, 0, 1);
+                    break;
+                // Z-Axis
+                case (Axis.Z):
+                    rotationMatrix = new Matrix3D(Math.Cos(theta), -(Math.Sin(theta)), 0, 0,
+                                                     Math.Sin(theta), Math.Cos(theta), 0, 0,
+                                                     0, 0, 1, 0,
+                                                     0, 0, 0, 1);
+                    break;
+                default:
+                    rotationMatrix = Matrix3D.Identity;
+                    break;
+            }
+            
+            applyRotation(rotationMatrix);
+        }
+
+        public void subtractFromPointAxis(double value, Axis axis)
+        {
+            int numberOfPoints = points.Count;
+            switch (axis)
+            {
+                case (Axis.X):
+                    for (int i = 0; i < numberOfPoints; i++)
+                    {
+                        points[i] = new Point3D(points[i].X - value, points[i].Y, points[i].Z);
+                    }
+                    break;
+                case (Axis.Y):
+                    for (int i = 0; i < numberOfPoints; i++)
+                    {
+                        points[i] = new Point3D(points[i].X, points[i].Y - value, points[i].Z);
+                    }
+                    break;
+                case (Axis.Z):
+                    for (int i = 0; i < numberOfPoints; i++)
+                    {
+                        points[i] = new Point3D(points[i].X, points[i].Y, points[i].Z - value);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error with point cloud subtraction");
+                    break;
+            }
+        }
+
+        public void rotateOnSpot(double degrees, Axis axis)
+        {
+            Point3D centroid = getCentroid();
+            subtractCentroid(centroid);
+            rotatePointCloud(degrees, axis);
+            addCentroid(centroid);
+        }
+        public void rotateOnSpot(double degrees, Axis axis, Point3D centroid)
+        {
+            subtractCentroid(centroid);
+            rotatePointCloud(degrees, axis);
+            addCentroid(centroid);
+        }
+    }
+}
--- BodyScanner/ConsoleKinect/ConsoleKinect/Program.cs
+++ BodyScanner/ConsoleKinect/ConsoleKinect/Program.cs
@@ -104,139 +104,25 @@ namespace ConsoleKinect
                             }
                         }
                     }
-
                 }
             }
         }
-        private Point3D getCentroid(List<Point3D> points)
-        {
-            double xAccumulator = 0.0, yAccumulator = 0.0, zAccumulator = 0.0, xMean = 0.0, yMean = 0.0, zMean = 0.0;
-            int numberOfPoints = points.Count;
-            foreach (Point3D point in points)
-            {
-                xAccumulator += point.X;
-                yAccumulator += point.Y;
-                zAccumulator += point.Z;
-            }
-            xMean = xAccumulator / numberOfPoints;
-            yMean = yAccumulator / numberOfPoints;
-            zMean = zAccumulator / numberOfPoints;
-            return (new Point3D(xMean, yMean, zMean));
-        }
-        private void subtractCentroid(List<Point3D> points)
-        {
-            int numberOfPoints = points.Count;
-            Point3D centroid = getCentroid(points);
-            for (int i = 0; i < numberOfPoints; i++)
-            {
-                points[i] = (Point3D)Point3D.Subtract(points[i], centroid);
-            }
-        }
-        private void subtractCentroid(List<Point3D> points, Point3D centroid)
-        {
-            int numberOfPoints = points.Count;
-            for(int i = 0; i < numberOfPoints; i++)
-            {
-                points[i] = (Point3D) Point3D.Subtract(points[i], centroid);
-            }
-        }
-        private void addCentroid(List<Point3D> points)
-        {
-            Point3D centroid = getCentroid(points);
-            int numberOfPoints = points.Count;
-            for (int i = 0; i < numberOfPoints; i++)
-            {
-                points[i] = Point3D.Add(points[i], (Vector3D)centroid);
-            }
-        }
-        private void addCentroid(List<Point3D> points, Point3D centroid)
-        {
-            int numberOfPoints = points.Count;
-            for (int i = 0; i < numberOfPoints; i++)
-            {
-                points[i] = Point3D.Add(points[i], (Vector3D)centroid);
-            }
-        }
-
-        private void rotatePointCloud(List<Point3D> points, double degrees, int axis)
-        {
-            double theta = (Math.PI / 180) * degrees;
-            Matrix3D rotationMatrix;
-            switch(axis)
-            {
-                // X-Axis
-                case(0):
-                    rotationMatrix = new Matrix3D(1, 0, 0, 0,
-                                                     0, Math.Cos(theta), -(Math.Sin(theta)), 0,
-                                                     0, Math.Sin(theta), Math.Cos(theta), 0,
-                                                     0, 0, 0, 1);
-                    break;
-                // Y-Axis
-                case(1):
-                    rotationMatrix = new Matrix3D(Math.Cos(theta), 0, -(Math.Sin(theta)), 0,
-                                                     0, 1, 0, 0,
-                                                     Math.Sin(theta), 0, Math.Cos(theta), 0,
-                                                     0, 0, 0, 1);
-                    break;
-                // Z-Axis
-                case(2):
-                    rotationMatrix = new Matrix3D(Math.Cos(theta), -(Math.Sin(theta)), 0, 0,
-                                                     Math.Sin(theta), Math.Cos(theta), 0, 0,
-                                                     0, 0, 1, 0,
-                                                     0, 0, 0, 1);
-                    break;
-                default:
-                    rotationMatrix = Matrix3D.Identity;
-                    Console.WriteLine("Axis value out of range -> Using Identity Matrix for rotation");
-                    break;
-            }
-            // Applying rotation to each point
-            for (int i = 0; i < points.Count; i++)
-            {
-                points[i] = rotationMatrix.Transform(points[i]);
-            }
-        }
         
-        private void subtractFromPointAxis(List<Point3D> points, double value, int axis)
-        {
-            int numberOfPoints = points.Count;
-            switch(axis)
-            {
-                case(0):
-                    for (int i = 0; i < numberOfPoints; i++)
-                    {
-                        points[i] = new Point3D(points[i].X - value, points[i].Y, points[i].Z);
-                    }
-                    break;
-                case(1):
-                    for (int i = 0; i < numberOfPoints; i++)
-                    {
-                        points[i] = new Point3D(points[i].X, points[i].Y - value, points[i].Z);
-                    }
-                    break;
-                case(2):
-                    for (int i = 0; i < numberOfPoints; i++)
-                    {
-                        points[i] = new Point3D(points[i].X, points[i].Y, points[i].Z - value);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Axis out of range -> subtraction void");
-                    break;
-            }
-        }
-
         private void pointCloud(DepthFrame df, ColorFrame cf, BodyIndexFrame bif)
         {
             Console.WriteLine("Creating point Cloud");
+
+            /* 
+             * used to calculate centroid, as well as lowest x value for later on -> 
+             * saves us looping later, though PointCloud methods do allow you to do that
+            */
             int numberOfPoints = 0;
-
             double xAccumulator = 0.0, yAccumulator = 0.0, zAccumulator = 0.0, xMean = 0.0, yMean = 0.0, zMean = 0.0, xMinimum = (1/0.0);
-            List<Point3D> pointList = new List<Point3D>();
 
+            // LEGACY StringBuilders -> remove once no longer needed
             StringBuilder pc_ply = new StringBuilder(); // This is for the PLY file
             StringBuilder pc_csv = new StringBuilder(); // This is for the CSV file
-            StringBuilder transformed_pc_ply = new StringBuilder(); // This is for the transformed PLY file
+
             FrameDescription depthFrameDes = df.FrameDescription;
             int depthFrameWidth = depthFrameDes.Width;
             int depthFrameHeight = depthFrameDes.Height;
@@ -249,7 +135,10 @@ namespace ConsoleKinect
 
             coordinateMapper.MapDepthFrameToColorSpace(depthFrameData, colorPoints);
             coordinateMapper.MapDepthFrameToCameraSpace(depthFrameData, cameraPoints);
-            
+
+            // Create new point cloud for storing points and operating on later
+            PointCloud pointCloud = new PointCloud();
+
             // loop over each row and column of the depth
             for (int y = 0; y < depthFrameHeight; y++)
             {
@@ -260,84 +149,83 @@ namespace ConsoleKinect
 
                     byte human = bodyPoints[depthIndex];
 
-                    if (human != 0xff) // Check if pixel is part of human Body
+                    if (human == 0xff)
+                        continue;
+
+                    CameraSpacePoint p = this.cameraPoints[depthIndex];
+
+                    // retrieve the depth to color mapping for the current depth pixel
+                    ColorSpacePoint colorPoint = this.colorPoints[depthIndex];
+
+                    byte r = 0;
+                    byte g = 0;
+                    byte b = 0;
+
+                    // make sure the depth pixel maps to a valid point in color space
+                    int colorX = (int)Math.Floor(colorPoint.X + 0.5);
+                    int colorY = (int)Math.Floor(colorPoint.Y + 0.5);
+                    if ((colorX >= 0) && (colorX < colorFrameWidth) && (colorY >= 0) && (colorY < colorFrameHeight))
                     {
-                        CameraSpacePoint p = this.cameraPoints[depthIndex];
+                        // calculate index into color array
+                        int colorIndex = ((colorY * colorFrameWidth) + colorX) * this.bytesPerPixel;
 
-                        // retrieve the depth to color mapping for the current depth pixel
-                        ColorSpacePoint colorPoint = this.colorPoints[depthIndex];
+                        // set source for copy to the color pixel
+                        int displayIndex = depthIndex * this.bytesPerPixel;
 
-                        byte r = 0;
-                        byte g = 0;
-                        byte b = 0;
+                        b = this.colorFrameData[colorIndex++];
+                        g = this.colorFrameData[colorIndex++];
+                        r = this.colorFrameData[colorIndex++];
+                    }
 
-                        // make sure the depth pixel maps to a valid point in color space
-                        int colorX = (int)Math.Floor(colorPoint.X + 0.5);
-                        int colorY = (int)Math.Floor(colorPoint.Y + 0.5);
-                        if ((colorX >= 0) && (colorX < colorFrameWidth) && (colorY >= 0) && (colorY < colorFrameHeight))
+                    if (!(Double.IsInfinity(p.X)) && !(Double.IsInfinity(p.Y)) && !(Double.IsInfinity(p.Z)))
+                    {
+                        if (p.X < depthLimit && p.Y < depthLimit && p.Z < depthLimit)
                         {
-                            // calculate index into color array
-                            int colorIndex = ((colorY * colorFrameWidth) + colorX) * this.bytesPerPixel;
-
-                            // set source for copy to the color pixel
-                            int displayIndex = depthIndex * this.bytesPerPixel;
-
-                            b = this.colorFrameData[colorIndex++];
-                            g = this.colorFrameData[colorIndex++];
-                            r = this.colorFrameData[colorIndex++];
-
-                        }
-
-
-                        if (!(Double.IsInfinity(p.X)) && !(Double.IsInfinity(p.Y)) && !(Double.IsInfinity(p.Z)))
-                        {
-                            if (p.X < depthLimit && p.Y < depthLimit && p.Z < depthLimit)
+                            Point3D scaledPoint = new Point3D(p.X * unitScale, p.Y * unitScale, p.Z * unitScale);
+                            if (NO_COLOUR == true)
                             {
-                                Point3D scaledPoint = new Point3D(p.X * unitScale, p.Y * unitScale, p.Z * unitScale);
-                                if (NO_COLOUR == true)
-                                {
-                                    pc_ply.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z));
-                                    pc_csv.Append(String.Format(CultureInfo.InvariantCulture, "{0},{1},{2}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z));
-                                    pointList.Add(scaledPoint);
-                                }
-                                else
-                                {
-                                    pc_ply.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3} {4} {5}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z, r, g, b));
-                                    pointList.Add(scaledPoint);
-                                }
-                               
-                                xAccumulator += scaledPoint.X;
-                                yAccumulator += scaledPoint.Y;
-                                zAccumulator += scaledPoint.Z;
-                                numberOfPoints++;
+                                // legacy StringBuilder appends -> remove once no longer needed
+                                pc_ply.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z));
+                                pc_csv.Append(String.Format(CultureInfo.InvariantCulture, "{0},{1},{2}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z));
 
-                                if(scaledPoint.X < xMinimum)
-                                {
-                                    xMinimum = scaledPoint.X;
-                                }
+                                pointCloud.addPoint(scaledPoint);
+                            }
+                            else
+                            {
+                                pc_ply.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3} {4} {5}\n", scaledPoint.X, scaledPoint.Y, scaledPoint.Z, r, g, b));
+
+                                pointCloud.addPoint(scaledPoint);
+                            }
+
+                            xAccumulator += scaledPoint.X;
+                            yAccumulator += scaledPoint.Y;
+                            zAccumulator += scaledPoint.Z;
+                            numberOfPoints++;
+
+                            if (scaledPoint.X < xMinimum)
+                            {
+                                xMinimum = scaledPoint.X;
                             }
                         }
-                    } 
-                }
-            }
+                    }
+                } // end of for(int x..) loop over points
+            } // end of for(int y..) loop over points
 
             xMean = xAccumulator / numberOfPoints;
             yMean = yAccumulator / numberOfPoints;
             zMean = zAccumulator / numberOfPoints;
+
+            // centroid calculated on the fly so we don't have to loop again unnecessarily
             Point3D centroid = new Point3D(xMean, yMean, zMean);
 
-            subtractFromPointAxis(pointList, xMinimum, 0);
-            subtractCentroid(pointList, centroid);
-            rotatePointCloud(pointList, 180, 1);
-            addCentroid(pointList, centroid); // Using the same centroid as before to reverse the subtraction
-
-            for(int i = 0; i < pointList.Count; i++)
-            {
-                transformed_pc_ply.Append(String.Format(CultureInfo.InvariantCulture, "{0} {1} {2}\n", pointList[i].X, pointList[i].Y, pointList[i].Z));
-            }
-
-            String header = "";
+            pointCloud.subtractFromPointAxis(xMinimum, 0);
+            pointCloud.rotateOnSpot(180, PointCloud.Axis.Y);
             
+            String transformed_pc_ply = pointCloud.ToString();
+
+            // legacy header -> remove once no longer needed and use pointCloud.ToString() which uses default header, or use pointCloud.ToString(header)
+            // note that the new PointCloud doesn't support
+            String header = "";
             if (NO_COLOUR == true)
             {
                 header = "ply \n" +
@@ -362,9 +250,13 @@ namespace ConsoleKinect
                             "end_header \n";
             }
             
+            // LEGACY outputs -> remove once no longer necessary
             saveFile(header + pc_ply.ToString(), "ply", "pointcloud");
             saveFile(pc_csv.ToString(), "txt", "pointcloud_csv");
-            saveFile(header + transformed_pc_ply.ToString(), "ply", "transformed_pointcloud");
+
+            // new output using PointCloud class
+            saveFile(transformed_pc_ply, "ply", "transformed_pointcloud");
+
             Environment.Exit(0); //Quit the application -- not really thread safe or a good practice
         }
 
